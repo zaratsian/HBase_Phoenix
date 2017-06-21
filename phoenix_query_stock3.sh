530a39dbb8656584fe26714603d93fd04fc18d78
Dan Zaratsian <dtz001@gmail.com>
2017-06-21 08:24:49 -0400

    Update phoenix_query_stock3.sh

--- phoenix_query_stock3.sh
+++ phoenix_query_stock3.sh
@@ -13,15 +13,18 @@ mktOpenDtm=$2   # "2017-05-01 09:30:00.000"
 RefDateTime=$3  # "2017-05-01 09:31:00.000"
 
 secs=$(date +%s --date="$RefDateTime")
-date_minus_30sec=$(date '+%Y-%m-%d %H:%M:%S.%3N' --date="@$((secs - 30))")
+WindowTimeBegin=$(date '+%Y-%m-%d %H:%M:%S.%3N' --date="@$((secs - 5))")
+WindowTimeEnd=$RefDateTime
 
 echo "select
-    ISNULL(AVG(cast(BID_LOT_QTY as bigint)),0) as WindowAvgBidQty,
-    ISNULL(AVG(cast(ASK_LOT_QTY as bigint)),0) as WindowAvgAskQty
+    AVG(BID_LOT_QTY) as WindowAvgBidQty,
+    AVG(ASK_LOT_QTY) as WindowAvgAskQty
     from MktData
     where SECURITY_SYMBOL = '$Symbol'
-        and SOURCE_DATETIME >= '$WindowTimeBegin'
-        and SOURCE_DATETIME < '$WindowTimeEnd'
-    limit 1;" > phoenixquery3.sql
+        and SOURCE_DATETIME >= TO_DATE('$WindowTimeBegin')
+        and SOURCE_DATETIME < TO_DATE('$WindowTimeEnd')
+    limit 10;" > phoenixquery3.sql
+
+#cat phoenixquery3.sql
 
 python /usr/hdp/2.6.1.0-129/phoenix/bin/sqlline.py biren-hbase0.field.hortonworks.com,biren-hbase1.field.hortonworks.com,biren-hbase2.field.hortonworks.com:2181:/hbase-unsecure phoenixquery3.sql
